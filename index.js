adca671fdd0fe2013f97d00ef7a726a288f01678
Harrison Cates <harrison.cates@vanderbilt.edu>
2018-12-12 14:03:50 -0800

    Token Working

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 let express = require("express");
+let axios = require('axios');
 let app = express();
 
 app.get('/',function(req,res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/token', function(req,res) {
+    var auth = 'Basic ' + Buffer.from('9e5a220335ce4a0f84976771850e0c55:61254f20eea94b1ba1b79234ce60b83f').toString('base64');
+
+    axios({
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        headers: {
+            'Authorization': auth
+        },
+        params: {
+            grant_type: 'client_credentials'
+        }
+    })
+        .then(function(response) {
+            console.log("Token Received.");
+            res.send(response.data.access_token);
+        })
+        .catch(function(error) {
+            console.log("Token Not Received.");
+        });
+});
+
 app.listen(8080);
 
-console.log("Listening on Port 8080");
+console.log("MusicRec listening on 8080");
